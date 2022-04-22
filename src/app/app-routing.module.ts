0d9a70f9563a27bdee750289a2bfd534e09ba195
rodrigosouza07 <rodrigo_souza07@outlook.com>
2022-04-21 23:31:37 -0400

    importando MateriasComponent no roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MateriasComponent } from './materias/materias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'materias', component: MateriasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
